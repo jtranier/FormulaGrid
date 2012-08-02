134cba2c7068160524e1a8656aeb72c3778d8979
John Tranier <tranier@gmail.com>
2012-08-02 21:35:50 +0200

    #1 Creation du projet - completion .gitignore

---
+++ test/unit/formulagrid/CarServiceTests.groovy
@@ -0,0 +1,17 @@
+package formulagrid
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(CarService)
+class CarServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
